a3d50debaae44d9cba557b6e74524a4f0185c750
5almiakki <jpui9635@gmail.com>
2025-07-18 15:39:58 +0900

    Initialize project

---
+++ OAuth2ClientJwtPractice/src/test/java/s5almiakki/oauth2clientjwtpractice/OAuth2ClientJwtPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package s5almiakki.oauth2clientjwtpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2ClientJwtPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
