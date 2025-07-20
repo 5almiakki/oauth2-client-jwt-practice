aaad8bd0d84b7f4c3bfdf82aae260a2ba706f2ab
5almiakki <jpui9635@gmail.com>
2025-07-20 18:38:08 +0900

    Configure web security

---
+++ OAuth2ClientJwtPractice/src/main/java/s5almiakki/oauth2clientjwtpractice/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package s5almiakki.oauth2clientjwtpractice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .formLogin(formLogin -> formLogin.disable())
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .oauth2Login(Customizer.withDefaults())
+                .authorizeHttpRequests(httpRequest -> httpRequest
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+}
