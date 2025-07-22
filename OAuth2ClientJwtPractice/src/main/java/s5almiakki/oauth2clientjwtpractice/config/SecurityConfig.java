adda8e342a93da85568bf4d399c8a68686d2889f
5almiakki <jpui9635@gmail.com>
2025-07-22 16:11:17 +0900

    Add CustomOAuth2UserService

--- OAuth2ClientJwtPractice/src/main/java/s5almiakki/oauth2clientjwtpractice/config/SecurityConfig.java
+++ OAuth2ClientJwtPractice/src/main/java/s5almiakki/oauth2clientjwtpractice/config/SecurityConfig.java
@@ -1,16 +1,20 @@
 package s5almiakki.oauth2clientjwtpractice.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import s5almiakki.oauth2clientjwtpractice.service.CustomOAuth2UserService;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -18,7 +22,9 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .formLogin(formLogin -> formLogin.disable())
                 .httpBasic(httpBasic -> httpBasic.disable())
-                .oauth2Login(Customizer.withDefaults())
+                .oauth2Login(oAuth2Login -> oAuth2Login
+                        .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint
+                                .userService(customOAuth2UserService)))
                 .authorizeHttpRequests(httpRequest -> httpRequest
                         .requestMatchers("/").permitAll()
                         .anyRequest().authenticated())
