c2d9b36e454bd630e49a410707d69585caf3a6b7
5almiakki <jpui9635@gmail.com>
2025-07-22 17:13:11 +0900

    Add logic to create and persist OAuth2 user info

--- OAuth2ClientJwtPractice/src/main/java/s5almiakki/oauth2clientjwtpractice/service/CustomOAuth2UserService.java
+++ OAuth2ClientJwtPractice/src/main/java/s5almiakki/oauth2clientjwtpractice/service/CustomOAuth2UserService.java
@@ -1,18 +1,28 @@
 package s5almiakki.oauth2clientjwtpractice.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import s5almiakki.oauth2clientjwtpractice.dto.CustomOAuth2User;
 import s5almiakki.oauth2clientjwtpractice.dto.NaverOAuth2Response;
 import s5almiakki.oauth2clientjwtpractice.dto.OAuth2Response;
+import s5almiakki.oauth2clientjwtpractice.entity.UserEntity;
+import s5almiakki.oauth2clientjwtpractice.repository.UserRepository;
 
+import java.util.Optional;
+
+@RequiredArgsConstructor
 @Slf4j
 @Service
+@Transactional
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -28,6 +38,15 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
                 return null;
         }
         String username = oAuth2Response.getProvider() + "_" + oAuth2Response.getProviderId();
-        return new CustomOAuth2User(oAuth2User.getName(), "ROLE_USER", username);
+        String name = oAuth2Response.getName();
+        Optional<UserEntity> oUser = userRepository.findByUsername(username);
+        UserEntity user;
+        if (oUser.isEmpty()) {
+            user = userRepository.save(new UserEntity(username, name, "ROLE_USER"));
+        } else {
+            user = oUser.get();
+            user.updateName(name);
+        }
+        return new CustomOAuth2User(name, user.getRole(), username);
     }
 }
