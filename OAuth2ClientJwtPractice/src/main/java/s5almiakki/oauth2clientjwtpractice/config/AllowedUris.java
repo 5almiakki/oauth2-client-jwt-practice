37af49f8181838daf681e77c9df5b191880b92d8
5almiakki <jpui9635@gmail.com>
2025-07-23 15:18:22 +0900

    Add validation method to AllowedUris

--- OAuth2ClientJwtPractice/src/main/java/s5almiakki/oauth2clientjwtpractice/config/AllowedUris.java
+++ OAuth2ClientJwtPractice/src/main/java/s5almiakki/oauth2clientjwtpractice/config/AllowedUris.java
@@ -5,7 +5,10 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -14,4 +17,21 @@ import java.util.List;
 public class AllowedUris {
 
     private List<String> allowedUris;
+
+    public void validateRedirectUri(String uri) {
+        try {
+            URI redirectUri = new URI(uri);
+            for (String allowed : allowedUris) {
+                URI allowedUri = new URI(allowed);
+                if (Objects.equals(redirectUri.getHost(), allowedUri.getHost())
+                        && Objects.equals(redirectUri.getScheme(), allowedUri.getScheme())
+                        && (redirectUri.getPort() != -1 || redirectUri.getPort() != allowedUri.getPort())) {
+                    return;
+                }
+            }
+        } catch (URISyntaxException e) {
+            throw new IllegalArgumentException("Malformed redirect URI: " + uri, e);
+        }
+        throw new IllegalArgumentException("Invalid redirect URI: " + uri);
+    }
 }
