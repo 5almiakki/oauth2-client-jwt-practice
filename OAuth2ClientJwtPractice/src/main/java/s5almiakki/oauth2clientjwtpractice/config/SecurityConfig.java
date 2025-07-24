3a1d0123293041fb115da2c1bf322854786879cc
5almiakki <jpui9635@gmail.com>
2025-07-24 15:35:56 +0900

    Prevent infinite redirection by modifying filter chain

--- OAuth2ClientJwtPractice/src/main/java/s5almiakki/oauth2clientjwtpractice/config/SecurityConfig.java
+++ OAuth2ClientJwtPractice/src/main/java/s5almiakki/oauth2clientjwtpractice/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -55,7 +56,7 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterAfter(jwtFilter, OAuth2LoginAuthenticationFilter.class)
                 .addFilterBefore(oAuth2RedirectUriCookieFilter, CorsFilter.class)
                 .build();
     }
